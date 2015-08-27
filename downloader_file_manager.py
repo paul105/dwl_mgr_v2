66c1e2c138dc899122c2b46f23a76bb02fd59b1d
Pawel N <graficiak@gmail.com>
2015-08-27 22:11:13 +0200

    another updates

--- downloader_file_manager.py
+++ downloader_file_manager.py
@@ -6,24 +6,29 @@ import shutil
 import json
 
 
-def downloader(i, req, dir, dict):               #funkcja dziecko, przekazujemy "id", "Request object", oraz katalog roboczy
+def downloader(child_number, request_object, temporary_directory, manager_dictionary):
+    manager_dictionary[child_number]['time_start']  = time.time()
     # a=0
     # if not req.has_header("Range"): a=1
-    data = urllib2.urlopen(req)
+    data = urllib2.urlopen(request_object)
     CHUNK = 1024*512                #ilosc danych czytana jednorazowo
-    f_path = dir+"\\file"+str(i)
+    f_path = temporary_directory+"\\file"+str(child_number)
     with open(f_path,"wb") as output:
         while True:
             _time_start = time.time()
             chunk = data.read(CHUNK)    #odczytujemy pobrana jednostke danych i zapisujemy, jak wszystko odczytane ->break
             _time = time.time()-_time_start
             if not chunk:
+                manager_dictionary['time_stop'] = time.time()
                 # if a == 1:
                 #     del_and_combine(dir+"\\..", dir,str(dir).split(".",1)[1],1)
                 break
             output.write(chunk)
-            dict[i] = {'data' : CHUNK, 'time' : _time}
+            manager_dictionary[child_number]['data'] = CHUNK
+            manager_dictionary[child_number]['time'] = _time
+            # print 'Child {}, CHUNK = {}, time = {}'.format(child_number, CHUNK, _time)
             CHUNK = int((CHUNK*0.3)+(len(chunk)/_time)*0.7)
+
 
 
 
--- supervisor_api.py
+++ supervisor_api.py
@@ -33,65 +33,84 @@ class Supervisor_manager_api(myFile_api.MyFile):
                 request_object = urllib2.Request(self.url[0], headers=headers)
             self.process_pool.apply_async(func=downloader_file_manager.downloader,
                                      args=[0, request_object,
-                                           self.temporary_directory, self.child_dict])
+                                           self.temporary_directory, self.manager_dictionary])
         return self.process_pool
 
     def downloader_one_url(self):
         from threading import Thread
-        # self.process_pool = Pool(self.parts)
         self.process_pool = []
         man = Manager()
-        self.child_dict = man.dict()
-        self.child_dict = {}
+        self.manager_dictionary = man.dict()
+        self.manager_dictionary = {}
+        for i in range(self.parts):
+            self.manager_dictionary[i] = {
+                'data'  : None,
+                'time' : None,
+                'time_start' : None,
+                'time_stop' : None,
+                'part_size' : None
+            }
 
         self.set_data_block()
         self.prepare_temporary_files()
-        for download_process_number in range(0, self.parts):
+        for download_process_number in range(self.parts):
             start = download_process_number * int(self.data_block)
             if not download_process_number == self.parts -1:
                 stop = start + int(self.data_block) -1
             else:
                 stop = int(self.size)
+            self.manager_dictionary[download_process_number]['part_size'] = int(stop-start)
             headers['Range'] = "bytes={}-{}".format(str(start),str(stop))
             request_object = urllib2.Request(self.url[0], headers=headers)
             thread = Thread(target=downloader_file_manager.downloader,
                             args=[download_process_number, request_object,
-                                           self.temporary_directory, self.child_dict])
+                                           self.temporary_directory, self.manager_dictionary])
             thread.start()
             self.process_pool.insert(download_process_number,thread)
         return self.process_pool
 
-    def set_text(self, number, part_number, s):
-        divider = 1
-        text = ('{:.2f}/{:.2f} ({:2.2f})% {:.2f} B/s'.format(
-                    s/divider, self.data_block/divider,
-                    ((s*100)/self.data_block), 0))
-        try:
-            data = float(self.child_dict[part_number]['data'])
-            time = float(self.child_dict[part_number]['time'])
-            speed = data/time
-            if number == 0 :
-                divider = 1
-            elif number == 1:
-                divider = 1024
-            elif number == 2:
-                divider = (1024**2)
+    def get_partial_file_size_and_set_text(self, part_number):
+        actual_part_size = float(os.path.getsize(os.path.join(self.temporary_directory,'file{}'.format(part_number))))
+        part_size = self.manager_dictionary[part_number]['part_size']
+        if int(actual_part_size) >= self.manager_dictionary[part_number]['part_size']-1:
+            self.manager_dictionary[part_number]['data'] = 0.0
+            self.manager_dictionary[part_number]['time'] = 1.0
 
-            if speed/1024 > 1 and speed/(1024**2) < 0:
-                text = ('{:.2f}/{:.2f} ({:2.2f})% {:.2f} KB/s'.format(
-                    s/divider, self.data_block/divider,
-                    ((s*100)/self.data_block), (speed/1024)
-                ))
-            elif speed/(1024**2) > 1:
-                text = ('{:.2f}/{:.2f} ({:2.2f})% {:.2f} MB/s'.format(
-                    s/divider, self.data_block/divider,
-                    ((s*100)/self.data_block), (speed/(1024**2))
-                ))
-            else:
-                text = ('{:.2f}/{:.2f} ({:2.2f})% {:.2f} B/s'.format(
-                    s/divider, self.data_block/divider,
-                    ((s*100)/self.data_block), speed
-                ))
+
+        if part_size/1024 <0 :
+            return self.set_text(1,part_number,actual_part_size)
+        elif 1000 > part_size/1024 > 1:
+            return self.set_text(2,part_number,actual_part_size)
+        elif part_size/(1024**2) > 1 :
+            return self.set_text(3,part_number,actual_part_size)
+        else:
+            return "ala ma kota"
+
+    def set_text(self, number, part_number, actual_part_size):
+        text = ('{:.2f}/{:.2f} ({:2.2f}%)   {:.2f} B/s'.format(
+                    actual_part_size, self.data_block,
+                    (actual_part_size*100)/self.data_block, 0))
+        try:
+            data = float(self.manager_dictionary[part_number]['data'])
+            time = float(self.manager_dictionary[part_number]['time'])
+            speed = data/time
+            if number == 1 :
+                divider = 1
+                text = ('{:.2f}/{:.2f} ({:2.2f}%)   {:.2f} B/s'.format(
+                    actual_part_size/divider, self.data_block/divider,
+                    (actual_part_size*100)/self.data_block, speed))
+
+            elif number == 2:
+                divider = 1024
+                text = ('{:.2f}/{:.2f} ({:2.2f}%)   {:.2f} KB/s'.format(
+                    actual_part_size/divider, self.data_block/divider,
+                    (actual_part_size*100)/self.data_block, speed/divider))
+
+            elif number == 3:
+                divider = (1024**2)
+                text = ('{:.2f}/{:.2f} ({:2.2f}%)   {:.2f} MB/s'.format(
+                    actual_part_size/divider, self.data_block/divider,
+                    (actual_part_size*100)/self.data_block, speed/divider))
         except:
             pass
         finally:
@@ -108,46 +127,26 @@ class Supervisor_manager_api(myFile_api.MyFile):
         ui_dl_handler.set_table_download_parts_info_rows(self.parts)
 
         while True:
-
             QtCore.QCoreApplication.processEvents()
             suma = 0
-            for part_number in range(0, self.parts):
-                suma += os.path.getsize(self.temporary_directory + "\\file" + str(part_number))
+            for part_number in range(self.parts):
+                suma += os.path.getsize(os.path.join(self.temporary_directory,'file{}'.format(part_number)))
             if int(suma) == int(self.size):
-                for part_number in range(0,self.parts):
-                    self.process_pool[part_number].join()
-                for part_number in range(0, self.parts):
-                    s = float(os.path.getsize(self.temporary_directory + "\\file" + str(part_number)))
-                    if s/1024 <0 :
-                        text = self.set_text(0,part_number,s)
-                    elif s/1024 > 1 and s/(1024**2) < 0 :
-                        text = self.set_text(1,part_number,s)
-                    elif s/(1024**2) > 1 :
-                        text = self.set_text(2,part_number,s)
+                [self.process_pool[part_number].join() for part_number in range(self.parts)]
+                for part_number in range(self.parts):
+                    text = self.get_partial_file_size_and_set_text(part_number)
                     item = QtGui.QTableWidgetItem()
                     item.setText(text)
                     item.setFlags(QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsDragEnabled|QtCore.Qt.ItemIsEnabled)
                     ui_dl_handler.table_download_parts_info.setItem(part_number,0,item)
-                # for part_number in range(0,self.parts):
-                #     s = float(os.path.getsize(self.temporary_directory + "\\file" + str(part_number)))
-                #     item = QtGui.QTableWidgetItem("%.2f/%.2f (100.00%%)" % (self.data_block/(1024**2), self.data_block/(1024**2)))
-                #     item.setFlags(QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsDragEnabled|QtCore.Qt.ItemIsEnabled)
-                #     ui_dl_handler.table_download_parts_info.setItem(part_number,0,item)
                 self.delete_and_combine_parts()
                 self.fileLogSave()
                 self.set_file_download_list()
                 ui_dl_handler.finish_button.setEnabled(True)
                 break
 
-            for part_number in range(0, self.parts):
-                s = float(os.path.getsize(self.temporary_directory + "\\file" + str(part_number)))
-                text = ''
-                if s/1024 < 0 :
-                    text = self.set_text(0,part_number,s)
-                elif s/1024 > 1 and s/(1024**2) < 0 :
-                    text = self.set_text(1,part_number,s)
-                elif s/(1024**2) > 1 :
-                    text = self.set_text(2,part_number,s)
+            for part_number in range(self.parts):
+                text = self.get_partial_file_size_and_set_text(part_number)
                 item = QtGui.QTableWidgetItem()
                 item.setText(text)
                 item.setFlags(QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsDragEnabled|QtCore.Qt.ItemIsEnabled)
